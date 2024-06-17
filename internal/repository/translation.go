64e0d9b89f646f5528f1d46ed8f57bf3e53e67ba
Yoga Raditya <radityanalaa@gmail.com>
2024-06-17 17:54:56 +0700

    fix: Add error handling and fix queries

--- internal/repository/translation.go
+++ internal/repository/translation.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/yogarn/arten/entity"
@@ -25,77 +27,95 @@ func NewTranslationRepository(db *sql.DB) ITranslationRepository {
 func (translationRepository *TranslationRepository) CreateTranslation(translation *entity.Translation) error {
 	stmt := `
 		INSERT INTO translations (id, origin_language, target_language, word, translate, updated_at, created_at) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
 	`
 
 	tx, err := translationRepository.db.Begin()
 	if err != nil {
-		return err
+		return fmt.Errorf("begin transaction: %v", err)
 	}
+
 	_, err = tx.Exec(stmt, translation.Id, translation.OriginLanguage, translation.TargetLanguage, translation.Word, translation.Translate, translation.UpdatedAt, translation.CreatedAt)
 	if err != nil {
 		tx.Rollback()
-		return err
+		return fmt.Errorf("exec statement: %v", err)
 	}
+
 	err = tx.Commit()
-	return err
+	if err != nil {
+		return fmt.Errorf("commit transaction: %v", err)
+	}
+
+	return nil
 }
 
 func (translationRepository *TranslationRepository) GetTranslation(id uuid.UUID) (*entity.Translation, error) {
 	stmt := `
 		SELECT id, origin_language, target_language, word, translate, updated_at, created_at
 		FROM translations
-		WHERE id = $1
+		WHERE id = ?
 	`
 
-	tx, err := translationRepository.db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	row := tx.QueryRow(stmt, id)
+	row := translationRepository.db.QueryRow(stmt, id)
 	translation := &entity.Translation{}
-	err = row.Scan(&translation.Id, &translation.OriginLanguage, &translation.TargetLanguage, &translation.Word, &translation.Translate, &translation.UpdatedAt, &translation.CreatedAt)
+	err := row.Scan(&translation.Id, &translation.OriginLanguage, &translation.TargetLanguage, &translation.Word, &translation.Translate, &translation.UpdatedAt, &translation.CreatedAt)
 	if err != nil {
-		return nil, err
+		if err == sql.ErrNoRows {
+			return nil, errors.New("no record found")
+		}
+		return nil, fmt.Errorf("scan row: %v", err)
 	}
+
 	return translation, nil
 }
 
 func (translationRepository *TranslationRepository) UpdateTranslation(id uuid.UUID, translation *entity.Translation) error {
 	stmt := `
 		UPDATE translations
-		SET origin_language = $1, target_language = $2, word = $3, translate = $4, updated_at = $5
-		WHERE id = $6
+		SET origin_language = ?, target_language = ?, word = ?, translate = ?, updated_at = ?
+		WHERE id = ?
 	`
 
 	tx, err := translationRepository.db.Begin()
 	if err != nil {
-		return err
+		return fmt.Errorf("begin transaction: %v", err)
 	}
+
 	_, err = tx.Exec(stmt, translation.OriginLanguage, translation.TargetLanguage, translation.Word, translation.Translate, translation.UpdatedAt, id)
 	if err != nil {
 		tx.Rollback()
-		return err
+		return fmt.Errorf("exec statement: %v", err)
 	}
+
 	err = tx.Commit()
-	return err
+	if err != nil {
+		return fmt.Errorf("commit transaction: %v", err)
+	}
+
+	return nil
 }
 
 func (translationRepository *TranslationRepository) DeleteTranslation(id uuid.UUID) error {
 	stmt := `
 		DELETE FROM translations
-		WHERE id = $1
+		WHERE id = ?
 	`
 
 	tx, err := translationRepository.db.Begin()
 	if err != nil {
-		return err
+		return fmt.Errorf("begin transaction: %v", err)
 	}
+
 	_, err = tx.Exec(stmt, id)
 	if err != nil {
 		tx.Rollback()
-		return err
+		return fmt.Errorf("exec statement: %v", err)
 	}
+
 	err = tx.Commit()
-	return err
+	if err != nil {
+		return fmt.Errorf("commit transaction: %v", err)
+	}
+
+	return nil
 }
