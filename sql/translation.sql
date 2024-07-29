9767d6274253e9129fa4c4aee9822500588f3ee0
Yoga Raditya <radityanalaa@gmail.com>
2024-07-29 18:17:48 +0700

    chore: Add sql query

---
+++ sql/translation.sql
@@ -0,0 +1,11 @@
+CREATE TABLE translations (
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    origin_language VARCHAR(255) NOT NULL,
+    target_language VARCHAR(255) NOT NULL,
+    word VARCHAR(255) NOT NULL,
+    translate VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
---
+++ sql/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    is_verified BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
