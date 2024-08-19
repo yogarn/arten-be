e282f62499b84906f99ab3aa2b33b2dacdb90494
Yoga Raditya <radityanalaa@gmail.com>
2024-08-19 18:03:47 +0700

    chore: Change database datatype of word to text

--- sql/translation.sql
+++ sql/translation.sql
@@ -3,7 +3,7 @@ CREATE TABLE translations (
     user_id VARCHAR(36) NOT NULL,
     origin_language VARCHAR(255) NOT NULL,
     target_language VARCHAR(255) NOT NULL,
-    word VARCHAR(255) NOT NULL,
+    word TEXT NOT NULL,
     translate VARCHAR(255) NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
